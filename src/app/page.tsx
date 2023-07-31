f99b4f86526a91ce7c7e0a90a8d8404d2e1412db
Lucas Seguessi <lucas.seguessi@gmail.com>
2023-07-31 08:59:47 -0300

    Ajustes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,37 @@
-"use client"
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useEffect, useState } from "react";
+import axios from "axios";
+import { headers } from "next/dist/client/components/headers";
 
 export default function Home() {
   const [cnpj, setCnpj] = useState<any>();
 
-  function getDataFromCNPJ() {
-    fetch(`https://www.receitaws.com.br/v1/cnpj/${cnpj}`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      });
+  async function getDataFromCNPJ() {
+    const resp = await axios.get(
+      `https://www.receitaws.com.br/v1/cnpj/${cnpj}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      }
+    );
+    console.log(resp);
   }
 
   return (
-    <div>
-      <label htmlFor="">Digite o CNPJ</label>
-      <input
-        type="text"
-        value={cnpj}
-        onChange={(e) => setCnpj(e.target.value)}
-      />
-      <button onClick={getDataFromCNPJ}></button>
+    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' , gap: 16}}>
+      <div>
+        <label htmlFor="">Digite o CNPJ</label>
+        <input
+          type="text"
+          value={cnpj}
+          onChange={(e) => setCnpj(e.target.value)}
+        />
+      </div>
+      <button onClick={getDataFromCNPJ}>Consultar</button>
     </div>
   );
 }
